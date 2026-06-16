2a52376c7bafc8dc7dfc35b09806eba3920ac2e6
NabeelZ-KA <nabeelzureikat27@kingsacademy.edu.jo>
2026-06-16 17:01:34 +0300

    fix?

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  base: "/3D-Comparative-Wall-Slicer",
 })
